1571ad7513107a40e4b0971c18d37ca38b0d200b
Denys <denys@Denyss-MacBook-Air.local>
2022-08-30 21:18:27 +0300

    fixed bags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,10 @@ function Home() {
         setItems(data)
         setIsLoading(false)
       })
+    window.scrollTo(0, 0)
   }, [])
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -28,7 +29,7 @@ function Home() {
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
           : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
       </div>
-    </>
+    </div>
   )
 }
 
